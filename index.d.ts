bf82294416188e619e89ac2a6fa22c741017ac96
dotvirus <33938500+dotvirus@users.noreply.github.com>
2020-11-06 14:12:04 +0100

    Add index.d.ts

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+declare function computeScores(str: string): number[];
+
+declare function isZalgo(str: string, threshold: number): boolean;
+
+declare function clean(str: string, threshold: number): string;
+
+export { computeScores, isZalgo, clean };
